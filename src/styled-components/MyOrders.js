fba782493536969cba43b45583eb0ad5cdf93744
cansuk <cansukaluc234@gmail.com>
2022-03-31 23:34:33 +0300

    bugfix

--- src/styled-components/MyOrders.js
+++ src/styled-components/MyOrders.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Font } from '../styled-components/Text'
+import { Text } from '../styled-components/Text'
 import { ColumnContainer } from '../styled-components/Layout'
 import { Link } from '../styled-components/Link';
 import { Variables } from '../styled-components/Variables';
@@ -10,8 +10,8 @@ const MyOrders = () => {
 
     return (
         <ColumnContainer justifyContent="center" alignItems="center" height="100vh">
-            <Font size={Size.xxlarge} weight={Weight.bold}>Under construction</Font>
-            <Font size={Size.xlarge} weight={Weight.normal}>You can always go back to the <Link to="/">homepage</Link></Font>
+            <Text size={Size.xxlarge} weight={Weight.bold}>Under construction</Text>
+            <Text size={Size.xlarge} weight={Weight.normal}>You can always go back to the <Link to="/">homepage</Link></Text>
         </ColumnContainer>
     )
 }
